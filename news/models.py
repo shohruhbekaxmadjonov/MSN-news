a7266c2b44ea8e56f55f44933cf9549de8839af5
Javohirbek <j.ahmedov.m@gmail.com>
2023-05-01 08:55:02 +0500

    commited in 0105

---
+++ news/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ['-id']
+
+
+class News(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    media = models.ImageField(upload_to="news-media/%Y/%m/%d/")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.id} news of {self.category}'
+
+    class Meta:
+        verbose_name = 'news'
+        verbose_name_plural = 'news'
+        ordering = ['-id']
